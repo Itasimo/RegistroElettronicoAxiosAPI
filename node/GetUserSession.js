aee998f58610ae056555328224f21425ddb85b34
Ita_simo <118686320+Itasimo@users.noreply.github.com>
2025-02-09 18:49:13 +0100

    Node: Aggiunta funzione mancante per il TitleCase del nome e cognome dello studente

--- node/GetUserSession.js
+++ node/GetUserSession.js
@@ -4,6 +4,23 @@ const VendorToken = require('../AxiosJSON/axios.json').VendorToken;
 
 /**
  * 
+ * https://stackoverflow.com/a/196991
+ * 
+ * Converts a string to title case.
+ * Each word in the string will have its first letter capitalized and the rest in lowercase.
+ *
+ * @param {string} str - The string to be converted to title case.
+ * @returns {string} - The converted title case string.
+ */
+function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
+    );
+}
+
+/**
+ * 
  * @param {String} CF   Codice Fiscale della scuola
  * @param {String} CU   Codice Utente
  * @param {String} PWD  Password
